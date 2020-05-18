f255aafe4c2cb3ba364899a61058407dd56669d9
Alejandro Sanchez <aalejo@gmail.com>
2020-05-18 05:19:19 +0000

    fixed bug in test

--- exercises/02-Hello-World/tests.js
+++ exercises/02-Hello-World/tests.js
@@ -10,47 +10,21 @@ let _log = console.log;
 // but we are also going to save what supposed to be the ouput of the console inside _buffer
 global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
 
-describe('All the javascript should match', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
-    });
-    afterEach(() => { jest.resetModules(); });
+test('console.log() function should be called with Hello World', function () {
 
-    it('console.log() function should be called with Hello World', function () {
+    /*
+        Here is how to mock the alert function:
+        https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
+    */
 
-        /*
-            Here is how to mock the alert function:
-            https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
-        */
+    //then I import the index.js (which should have the alert() call inside)
+    const file = require("./app.js");
 
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./app.js");
+    //Expect the console log to have been called with "Hello World" at least one
+    expect(console.log).toHaveBeenCalledWith("Hello World");
+    //and I expect the console.log to be already called just one time.
+    expect(console.log.mock.calls.length).toBe(1);
 
-        //Expect the console log to have been called with "Hello World" at least one
-        expect(console.log).toHaveBeenCalledWith("Hello World");
-        //and I expect the console.log to be already called just one time.
-        expect(console.log.mock.calls.length).toBe(1);
-
-        //You can also compare the entire console buffer (if there have been several console.log calls on the exercise)
-        expect(_buffer).toBe("Hello World\n");
-    });
-
-    it('console.log() function should be called with Hello World', function () {
-
-        /*
-            Here is how to mock the alert function:
-            https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
-        */
-
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./app.js");
-
-        //Expect the console log to have been called with "Hello World" at least one
-        expect(console.log).toHaveBeenCalledWith("Hello World");
-        //and I expect the console.log to be already called just one time.
-
-
-        //You can also compare the entire console buffer (if there have been several console.log calls on the exercise)
-        //expect(_buffer).toBe("Compare with the entire function buffer out");
-    });
+    //You can also compare the entire console buffer (if there have been several console.log calls on the exercise)
+    expect(_buffer).toBe("Hello World\n");
 });
--- exercises/06.4-Loop-from-the-half-to-the-end/tests.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/tests.js
@@ -11,8 +11,8 @@ it('Call the console.log function as many times as half of the amount of items i
     expect(console.log.mock.calls.length).toBe(variable.length/2);
 });
 
-it('Loop the array but jumping two items at a time instead of just one at a time', function () {
+it('Print in the console as you loop the array', function () {
     const app = rewire('./app.js');
     const variable = app.__get__('mySampleArray');
-    expect(_buffer).toMatch(variable.filter((n,i) => i < (variable.length / 2)).map(n => n+"\n").join(""));
+    expect(_buffer).toMatch(variable.filter((n,i) => i > (variable.length / 2)).map(n => n+"\n").join(""));
 });
