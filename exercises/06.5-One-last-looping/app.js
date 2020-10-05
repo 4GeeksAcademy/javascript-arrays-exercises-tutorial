6c9f221507524ac643cd2a5b13ed0b581b394ca5
Alejandro Sanchez <aalejo@gmail.com>
2020-10-05 18:49:09 +0000

    removed solution for 5.6

--- exercises/06.5-One-last-looping/app.js
+++ exercises/06.5-One-last-looping/app.js
@@ -1,7 +1,3 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
 //your code here
-//your code here
-for( i = mySampleArray.length-1; i>=0; i-- ){
-    console.log(mySampleArray[i]);
-}
