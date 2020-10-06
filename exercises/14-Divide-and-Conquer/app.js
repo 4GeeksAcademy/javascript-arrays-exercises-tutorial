62fe2dc02aa41030772042cba200c016bdf64bc4
Alejandro Sanchez <aalejo@gmail.com>
2020-10-06 14:36:54 -0400

    Update app.js

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,10 +1,6 @@
-let list_of_numbers = [4,	80,	85,	59,	37,25,	5,	64,	66,	81,20,	64,	41,	22,	76,76,	55,	96,	2,	68];
+let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
 
 // your code here
-function mergeTwoList(items){
-    const ods = items.filter(i => i % 2 !== 0)
-    const evens = items.filter(i => i % 2 === 0)
-    return ods.concat(evens)
-}
+
 
 console.log(mergeTwoList(list_of_numbers))
