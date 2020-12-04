f35f01c7e30001ae6145fc178e41c3645866b2be
Elvira Quiroz <elvira.quiroz.dp@gmail.com>
2020-12-04 21:50:38 +0000

    ex 19 fixed

--- exercises/19-And-One-and-a-Two-and-a-Three/app.js
+++ exercises/19-And-One-and-a-Two-and-a-Three/app.js
@@ -1,8 +1,8 @@
-contact = {
+let contact = {
     fullname: "Jane Doe",
     phone: "321-321-4321",
     email: "test@test.com"
 }
-for(key in contact){
+for(et key in contact){
     // Code goes here
 }
