38ae6b4e00e04f9191a3356bb9053ddfb8ac43d8
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-10-21 02:55:09 +0000

    Exercise 25 cleaned

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,24 +1,7 @@
-function lyricsGenerator(arr) {
-    var lyric = "";
-    var counter = 0;
-    
-    for(var i=0; i<arr.length; i++) {
-        
-        if (arr[i] === 0) {
-            lyric += "Boom "
-        } else if (arr[i] === 1) {
-            lyric += "Drop the base ";
-            counter += 1;
-            if (counter === 3) {
-            counter = 0;
-            lyric += "!!!Break the base!!! ";
-            }
-        }
-    }
-     return lyric; 
-}
+// Add your code here
 
-// test Data
+
+// Don't change anything bellow this line
 console.log(lyricsGenerator([0,0,1,1,0,0,0]))
 console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
 console.log(lyricsGenerator([0,0,0]))
