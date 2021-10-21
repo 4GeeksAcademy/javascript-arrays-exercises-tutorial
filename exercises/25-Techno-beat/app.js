4e3a598ae8de911eed2c90cb1f6345a2f81016cd
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-10-21 02:52:52 +0000

    Issue #191 fixed with the new corrections

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,7 +1,26 @@
+function lyricsGenerator(arr) {
+    var lyric = "";
+    var counter = 0;
+    
+    for(var i=0; i<arr.length; i++) {
+        
+        if (arr[i] === 0) {
+            lyric += "Boom "
+        } else if (arr[i] === 1) {
+            lyric += "Drop the base ";
+            counter += 1;
+            if (counter === 3) {
+            counter = 0;
+            lyric += "!!!Break the base!!! ";
+            }
+        }
+    }
+     return lyric; 
+}
 
 // test Data
-console.log(lyricsGenerator([0,0,1,1,0,0,0]).trim())
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]).trim())
-console.log(lyricsGenerator([0,0,0]).trim())
-console.log(lyricsGenerator([1,0,1]).trim())
-console.log(lyricsGenerator([1,1,1]).trim())
+console.log(lyricsGenerator([0,0,1,1,0,0,0]))
+console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
+console.log(lyricsGenerator([0,0,0]))
+console.log(lyricsGenerator([1,0,1]))
+console.log(lyricsGenerator([1,1,1]))
