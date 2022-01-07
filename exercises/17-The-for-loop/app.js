0c50e966acb9dd952ebc08d1935b417b2f9f76ce
George Mihov <53583283+gmihov001@users.noreply.github.com>
2022-01-07 11:30:41 -0500

    Update app.js

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,7 +1,7 @@
 var myArray = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423];
 
-for (var index in myArray){
+for (var number of myArray){
 
-	console.log(myArray[index]);
+	console.log(number);
 	
 }
