ec8984d3a534c6f3777f10b1a6006d92fddb7a67
Martín Suárez <ms.prgramador@gmail.com>
2022-01-26 06:25:49 +0000

    12-Delete-element

--- exercises/12-Delete-element/test.js
+++ exercises/12-Delete-element/test.js
@@ -5,48 +5,35 @@ const rewire = require("rewire");
 let _log = console.log;
 let _buffer = '';
 global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
-
+const file = rewire("./app.js");
 const app_content = fs.readFileSync(path.resolve(__dirname, './app.js'), 'utf8');
+const deletePerson = file.__get__('deletePerson');
+const people = file.__get__('people');
 
-test("Function deletePerson should exist", function(){
-  const file = rewire("./app.js");
-  const delPerson = file.__get__('deletePerson');
-  expect(delPerson).toBeTruthy();
+test("Function deletePerson should exist", function () {
+  expect(deletePerson).toBeTruthy();
 });
-test("Function deletePerson should return something", function(){
-  const file = rewire("./app.js");
-  const delPerson = file.__get__('deletePerson');
-  expect(delPerson()).toBeTruthy();
+test("Function deletePerson should return something", function () {
+  expect(deletePerson()).not.toBe(undefined)
 });
 test('console.log() function should have been called 3 times', function () {
-    //then I import the index.js (which should have the alert() call inside)
-    const file = require("./app.js");
-    expect(console.log.mock.calls.length).toBe(3);
+  //then I import the index.js (which should have the alert() call inside)
+  const file = require("./app.js");
+  expect(console.log.mock.calls.length).toBe(3);
 });
-test('The output in the console should match the one in the instructions!', function () {
-    const _app = rewire('./app.js');
-    
-    var _people = ['juan','ana','michelle','daniella','stefany','lucy','barak','emilio'];
 
-    function _deletePerson(personName)
-    {
-      //your code here
-      let newArray = [];
-      for (let i = 0; i < _people.length; i++){
-        if (_people[i] != personName){
-          newArray.push(_people[i])
-        }
-      }
-      return newArray
-    }
+test("If you call the deletePerson function with the name daniella, she cannot appear in the new list.", function () {
+  // expect(console.log).toHaveBeenCalledWith(deletePerson('daniella'))
+  expect(deletePerson('daniella')).not.toContain('daniella');
+});
+test("If you call the deletePerson function with the name juan, he cannot appear in the new list.", function () {
+  // expect(console.log).toHaveBeenCalledWith(deletePerson('juan'))
+  expect(deletePerson('juan')).not.toContain('juan');
+});
+
+test("If you call the deletePerson function with the name emilio, he cannot appear in the new list.", function () {
+  // expect(console.log).toHaveBeenCalledWith(deletePerson('emilio'))
+  expect(deletePerson('emilio')).not.toContain('emilio');
+});
 
 
-    let _test1 = _deletePerson('daniella')
-    let _test2 = _deletePerson('juan')
-    let _test3 = _deletePerson('emilio')
-
-    expect(console.log).toHaveBeenCalledWith(_test1);
-    expect(console.log).toHaveBeenCalledWith(_test2);
-    expect(console.log).toHaveBeenCalledWith(_test3);
-  
-  });
