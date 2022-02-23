5747eebb68f673687eebdbf1fba6a6a725ac06e4
Martín Suárez <ms.prgramador@gmail.com>
2022-02-23 04:26:02 +0000

    23-Parking-Lot

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -1,6 +1,13 @@
+
+
 let parking_state = [
-  [1,1,1],
-  [0,0,0],
-  [1,1,2]
+  [1, 0, 1, 1],
+  [0, 0, 0, 2],
+  [1, 1, 2, 1],
+  [2, 1, 1, 1]
 ]
+
+// Your code here
+
+
 console.log(getParkingLotState(parking_state))
