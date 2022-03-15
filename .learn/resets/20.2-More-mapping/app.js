9632a46c1efdfb48c6d11df0db9684ca64c1b270
Martín Suárez <ms.prgramador@gmail.com>
2022-03-15 14:41:31 +0000

    20.2-More-mapping

---
+++ .learn/resets/20.2-More-mapping/app.js
@@ -0,0 +1,7 @@
+
+
+var myNumbers = [23,234,345,4356234,243,43,56,2];
+
+// your code here
+
+console.log(newArray);
--- exercises/20.2-More-mapping/test.js
+++ exercises/20.2-More-mapping/test.js
@@ -10,8 +10,8 @@ const app_content = fs.readFileSync(path.resolve(__dirname, './app.js'), 'utf8')
 
 test("You should create a function named myFunction", function(){
   const file = rewire("./app.js");
-  const myFunc = file.__get__('myFunction');
-  expect(myFunc).toBeTruthy();
+  const myFunction = file.__get__('myFunction');
+  expect(myFunction).toBeTruthy();
 });
 test("You should create a variable named newArray", function(){
   const file = rewire("./app.js");
@@ -26,17 +26,16 @@ test('You have to use the console.log function', function () {
 
 test('The output in the console should match the one in the instructions!', function () {
     const _app = rewire('./app.js');
-   
-    var _myNumbers = [23,234,345,4356234,243,43,56,2];
+    const _myFunction = _app.__get__('myFunction');
+    var _numberTest = [23,234,345,4356234,243,43,56,2];
+    let _newArray = _numberTest.map(_myFunction);
+    expect(_buffer).toMatch([ 69, 702, 1035, 13068702, 729, 129, 168, 6 ].map(n => n).join(","));
+  });
 
-// your code here
-    function _myFunction(param){
-      return param * 3
-    }
-    let _newArray = _myNumbers.map(_myFunction)
-    
-    let _test = _newArray
-    expect(_buffer).toMatch(_test.map(n => n).join(","));
-    // expect(_buffer).toEqual(_test);
-
+  test('The solution should work with other arrays', function () {
+    const _app = rewire('./app.js');
+    const _myFunction = _app.__get__('myFunction');
+    var _numberTest = [44,125,90,98365637,77,1,43,0];
+    let _newArray = _numberTest.map(_myFunction);
+    expect(_newArray).toEqual([ 132, 375, 270, 295096911, 231, 3, 129, 0 ])
   });
