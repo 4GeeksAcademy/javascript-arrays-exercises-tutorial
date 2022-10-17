69956d56f89f619e70526e281d0ece7f2a15aa33
dsilva06 <diegosilva06092001@gmail.com>
2022-10-17 15:25:19 +0000

    tests

--- .learn/resets/04.1-Loop_from_one_to_seventeen/app.js
+++
@@ -1,4 +0,0 @@
-//change the conditions of the for loop
-for(let number = 0; number < 10; number++){
-    //print the number
-}  
--- .learn/resets/04.3-Add_items_to_array/app.js
+++
@@ -1,5 +0,0 @@
-let arr = [4,5,734,43,45];
-
-// Your code here, use the push function and the random function.
-
-console.log(arr);
