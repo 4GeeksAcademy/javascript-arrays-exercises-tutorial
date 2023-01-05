86769b8fd5f5a206796b3630441d5215cc513a9a
RaulAraque <luis.raul.araque2@gmail.com>
2023-01-05 00:39:32 +0000

    Correcion en el test

--- exercises/08.3-Sum-all-items/tests.js
+++ exercises/08.3-Sum-all-items/tests.js
@@ -24,10 +24,10 @@ it('Testing for [2,13,34,5] should return 54', function () {
     expect(_result).toEqual(54);
 });
 
-it('Testing for [12,3,37,5] should return 57', function () {
+it('Testing for [2,13,34,5] should return 54', function () {
     const _app = rewire('./app.js');
     const sumTheElements = _app.__get__('sumTheElements');
-    const _result = sumTheElements([12,3,37,5])
+    const _result = sumTheElements([2,13,34,5])
 
-    expect(_result).toEqual(57);
+    expect(_result).toEqual(54);
 });
