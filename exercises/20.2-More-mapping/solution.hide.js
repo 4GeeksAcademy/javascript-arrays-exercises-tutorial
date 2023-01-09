1d0f97705f883530ecfa07366b6d25d9c41aa6ce
dsilva06 <diegosilva06092001@gmail.com>
2023-01-09 20:48:19 +0000

    improvements

---
+++ exercises/20.2-More-mapping/solution.hide.js
@@ -0,0 +1,8 @@
+let myNumbers = [23,234,345,4356234,243,43,56,2];
+
+// your code here
+function myFunction(number) {
+    return number * 3
+}
+let newArray = myNumbers.map(myFunction)
+console.log(newArray);
