1fc9b30ec86eb303aba20b6e3be46017b14e1d01
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 17:38:50 +0200

    Update solution.hide.js

--- exercises/07.2-Letter-Counter/solution.hide.js
+++ exercises/07.2-Letter-Counter/solution.hide.js
@@ -1,19 +1,15 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
 
-// your code here
-for(let i in par){
+// Your code here
+for(let i in par) {
     const letter = par[i].toLowerCase();
-    console.log(letter);
     if(letter == " ") continue;
-    else if(counts[letter] == undefined){
-        console.log("Found "+letter+" for the first time")
+    else if(counts[letter] == undefined) {
         counts[letter] = 1;
     } 
-    else{
-        console.log("Found "+letter+" more than once")
+    else {
         counts[letter] = counts[letter] + 1;
-        
     }
 }
 
