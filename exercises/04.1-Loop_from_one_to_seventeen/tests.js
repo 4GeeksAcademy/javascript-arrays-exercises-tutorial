ce21114876f4502e0bc71543c251d65fbabe24f9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 13:37:31 +0200

    Update tests.js

--- exercises/04.1-Loop_from_one_to_seventeen/tests.js
+++ exercises/04.1-Loop_from_one_to_seventeen/tests.js
@@ -6,7 +6,7 @@ let _log = console.log;
 let _buffer = '';
 global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
 
-it('Call the console.log function on every cicle', function () {
+it('Call the console.log function on every cycle', function () {
     const app = require('./app.js');
     expect(console.log.mock.calls.length).toBe(17);
 });
@@ -16,6 +16,6 @@ test('You have to use a for loop', () => {
     expect(regex.test(file.toString())).toBeTruthy();
 })
 
-it('Print the numbers on the console on each loop cicle (inside the loop)', function () {
+it('Print the numbers on the console on each loop cycle (inside the loop)', function () {
     expect(_buffer).toMatch("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n");
 });
