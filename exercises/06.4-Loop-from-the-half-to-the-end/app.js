74cee258c09271618a376253a6fd892c0536d54c
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 15:22:21 +0200

    Update app.js

--- exercises/06.4-Loop-from-the-half-to-the-end/app.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/app.js
@@ -4,7 +4,7 @@ let initialValue = 0;
 let stopValue = 0;
 let increasingValue = 1;
 
-for(let i = initialValue; i<stopValue; i = i + increasingValue)
+for(let i = initialValue; i < stopValue; i = i + increasingValue)
 {
 	console.log(mySampleArray[i]);
 }
