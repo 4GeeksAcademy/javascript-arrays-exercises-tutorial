fe245e7e953b0e1872f5cbb4993e61040f0ab1a0
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 13:38:21 +0200

    Update solution.hide.js

--- exercises/04.1-Loop_from_one_to_seventeen/solution.hide.js
+++ exercises/04.1-Loop_from_one_to_seventeen/solution.hide.js
@@ -1,5 +1,5 @@
-// change the conditions of the for loop
+// Change the conditions of the for loop
 for(let number = 1; number <= 17; number++) {
-    // print the number
+    // Print the number
     console.log(number)
 }  
