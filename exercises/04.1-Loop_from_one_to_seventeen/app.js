5b95fd9223a3e38f8e359670f982d136af94e357
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 13:35:37 +0200

    Update app.js

--- exercises/04.1-Loop_from_one_to_seventeen/app.js
+++ exercises/04.1-Loop_from_one_to_seventeen/app.js
@@ -1,4 +1,4 @@
-//change the conditions of the for loop
-for(let number = 0; number < 10; number++){
-    //print the number
+// change the conditions of the for loop
+for(let number = 0; number < 10; number++) {
+    // print the number
 }  
