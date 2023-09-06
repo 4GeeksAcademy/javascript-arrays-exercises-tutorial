64cb4225c87757906e922a8ae3c31c479e2aaa33
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 18:16:44 +0200

    Update solution.hide.js

--- exercises/07.3-Flip-Array/solution.hide.js
+++ exercises/07.3-Flip-Array/solution.hide.js
@@ -1,9 +1,10 @@
 let arr = [45,67,87,23,5,32,60];
 
-//Your code here.
+// Your code here
 let flippedArray = []
-for(let i = arr.length - 1; i>= 0;i--){
+for(let i = arr.length - 1; i >= 0; i--) {
     let item = arr[i];
     flippedArray.push(item);
 }
+
 console.log(flippedArray)
