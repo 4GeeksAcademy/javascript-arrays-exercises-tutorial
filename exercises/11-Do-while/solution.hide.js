9a1889d2986109994f3ffd34bce7cca53df03643
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 21:44:41 +0200

    Update solution.hide.js

--- exercises/11-Do-while/solution.hide.js
+++ exercises/11-Do-while/solution.hide.js
@@ -1,13 +1,13 @@
 let i = 20;
 
 do {
-    // Magic goes here;
+    // Magic goes here
     if (i === 0) {
-        console.log("LIFTOFF")
+        console.log("LIFTOFF");
     } else if (i % 5 === 0) {
-        console.log(i + "!")
+        console.log(i + "!");
     } else {
-        console.log(i)
+        console.log(i);
     }
     i--;
 } while (i >= 0);
