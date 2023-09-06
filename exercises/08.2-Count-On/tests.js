3db37adbd063a0a39f2b4c226608bf263acc105b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 19:55:47 +0200

    Update tests.js

--- exercises/08.2-Count-On/tests.js
+++ exercises/08.2-Count-On/tests.js
@@ -8,20 +8,20 @@ global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
 
 const app_content = fs.readFileSync(path.resolve(__dirname, './app.js'), 'utf8');
 
-it('Use the console.log function once to print the variable hello', function () {
+it('Use the console.log function once to print the variable "hello"', function () {
     const app = require('./app.js');
     expect(console.log.mock.calls.length).toBe(1);
 });
 
-it("Use a for loop", function () {
+it('Use a "for" loop', function () {
     expect(app_content).toMatch(/for(\s*)\(/);
 });
 
-it("Use a condition 'if' statement only for items of type object", function () {
+it('Use a conditional "if" statement only for items of type \'object\'', function () {
     expect(app_content).toMatch(/if(\s*)\(/);
 });
 
-it("Do not cheat using the function filter", function () {
+it('Do not cheat using the function filter', function () {
     expect(app_content).not.toMatch(/\.filter(\s*)\(/);
 });
 
@@ -32,7 +32,7 @@ it("Do not cheat using the function filter", function () {
 //     expect(variable.filter(item => typeof(item) === "object")).toMatch(hello);
 // });
 
-it('The new array "hello" should only contain the "objects" in myArray', function () {
+it("The new array \"hello\" should only contain the 'object' types in myArray", function () {
     const _app = rewire('./app.js');
     const variable = _app.__get__('myArray');
     const hello = _app.__get__('hello');
