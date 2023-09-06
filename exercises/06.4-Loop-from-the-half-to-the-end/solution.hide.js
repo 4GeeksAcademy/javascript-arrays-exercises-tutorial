d5621b271ecdb90cc4c8a6087af9dd87212965af
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 15:21:21 +0200

    Update solution.hide.js

--- exercises/06.4-Loop-from-the-half-to-the-end/solution.hide.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/solution.hide.js
@@ -3,7 +3,7 @@ let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 let initialValue = mySampleArray.length / 2;
 let stopValue = mySampleArray.length;
 
-for(let i = initialValue; i<stopValue; i = i + 1)
+for(let i = initialValue; i < stopValue; i = i + 1)
 {
 	console.log(mySampleArray[i]);
 }
