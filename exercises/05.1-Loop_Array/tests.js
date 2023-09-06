52a2483bfaeffe05d337fed254957972e1612f32
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 15:12:22 +0200

    Update tests.js

--- exercises/05.1-Loop_Array/tests.js
+++ exercises/05.1-Loop_Array/tests.js
@@ -7,7 +7,7 @@ let _buffer = '';
 global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
 
 
-test('You have to use a for loop', () => {
+test('You have to use a "for" loop', () => {
     const file = fs.readFileSync(path.resolve(__dirname, './app.js'), 'utf8');
     const regex = /for\s*/gm
     expect(regex.test(file.toString())).toBeTruthy();
