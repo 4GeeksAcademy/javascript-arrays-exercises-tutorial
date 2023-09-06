41f5ae1c1df32fea6427a8c7b5360aa655a77d79
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 19:33:31 +0200

    Update app.js

--- exercises/08.2-Count-On/app.js
+++ exercises/08.2-Count-On/app.js
@@ -1,8 +1,8 @@
 let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 let hello = [];
-for(let index = 0; index < myArray.length; index++){
-    let element = myArray[index];
+for(let i = 0; i < myArray.length; i++){
+    let element = myArray[i];
     // MAGIC HAPPENS HERE
 }
 
