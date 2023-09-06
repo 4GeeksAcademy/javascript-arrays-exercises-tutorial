aabdb6b09152716690fe656b7e63ff5f50ec4b91
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 20:08:31 +0200

    Update solution.hide.js

--- exercises/08.3-Sum-all-items/solution.hide.js
+++ exercises/08.3-Sum-all-items/solution.hide.js
@@ -1,13 +1,12 @@
-function sumTheElements(sumArray){
+function sumTheElements(theArray) {
 	
-	sumArray =[2,13,34,5]
 	let total = 0;
-	
-	//your code here
-	for (let i = 0; i < sumArray.length; i++) {
-		
-		total += sumArray[i]
+	// Your code here
+	for (let i = 0; i < theArray.length; i++) {
+		total += theArray[i]
 	}
 
 	return total;
 }
+
+console.log(sumTheElements([2,13,34,5]))
