7a25f8df68957440e7d098855c5e1d916d3418c6
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 12:45:40 +0200

    Update solution.hide.js

--- exercises/02.1-Access_and_retrieve/solution.hide.js
+++ exercises/02.1-Access_and_retrieve/solution.hide.js
@@ -5,10 +5,10 @@ let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','satu
 
 
 // 1. print the 3rd item here
-console.log(myArray[2])
+console.log(myArray[2]);
 
 // 2. change 'thursday' value to null here
 myArray[4] = null;
 
 // 3. print the position of step 2
-console.log(myArray[4])
+console.log(myArray[4]);
