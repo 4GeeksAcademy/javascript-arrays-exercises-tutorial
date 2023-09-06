357d5c30abe94e4e6ec7328373e8e84c51e954b0
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 16:23:48 +0200

    Update tests.js

--- exercises/07.1-Finding-Waldo/tests.js
+++ exercises/07.1-Finding-Waldo/tests.js
@@ -13,7 +13,7 @@ it('You have to use the console.log function inside the loop', function () {
     expect(console.log.mock.calls.length > 0).toBe(true);
 });
 
-it('Use a for loop', function () {
+it('Use a "for" loop', function () {
     const app_content = fs.readFileSync(path.resolve(__dirname, './app.js'), 'utf8');
     expect(app_content).toMatch(/for(\s*)\(/);
 });
@@ -23,7 +23,7 @@ it('Use the toLowerCase function', function () {
     expect(app_content).toMatch(/\.toLowerCase(\s*)\(/);
 });
 
-it('Loop and add a conditional to print the position (i) where Waldo was fund', function () {
+it('Loop and add a conditional to print the position (i) where Waldo was found', function () {
     const _app = rewire('./app.js');
     const people = _app.__get__('people');
     const position = people.indexOf("Waldo");
