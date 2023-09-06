cd404b443b045d3b92aaf4981471020b7407238a
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-06 12:41:04 +0200

    Update app.js

--- exercises/02.1-Access_and_retrieve/app.js
+++ exercises/02.1-Access_and_retrieve/app.js
@@ -1,7 +1,7 @@
 // Declaring the array
 let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
 
-// 1. print the item here
+// 1. print the 3rd item here
 
 // 2. change the 'thursday' value to null here
 
