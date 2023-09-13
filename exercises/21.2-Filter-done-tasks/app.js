5953892ccb1e304df6bad7f90e384b758a240ef1
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 21:01:38 +0200

    Update app.js

--- exercises/21.2-Filter-done-tasks/app.js
+++ exercises/21.2-Filter-done-tasks/app.js
@@ -3,10 +3,10 @@ let tasks = [
 	{ label: 'Make the bed', done: false },
 	{ label: 'Have some fun', done: false },
 	{ label: 'Finish the replits', done: false },
-	{ label: 'Replit the finishes', done: true },
+	{ label: 'Finish my exercises', done: true },
 	{ label: 'Ask for a raise', done: false },
 	{ label: 'Read a book', done: true },
 	{ label: 'Make a trip', done: false }
 ];
 
-//your code here
+// Your code here
