dde7a127f3b8b735573033e829f303376a0437ae
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 19:28:02 +0200

    Update app.js

--- exercises/20.4-Map-data-types/app.js
+++ exercises/20.4-Map-data-types/app.js
@@ -1,7 +1,8 @@
 let arrayOfStrings = ['1', '5', '45', '34', '343', '34', 6556, 323];
 
 let newArray = arrayOfStrings.map(function(item) {
-	return item;
+	// Your code here
+	
 });
 
-
+console.log(newArray);
