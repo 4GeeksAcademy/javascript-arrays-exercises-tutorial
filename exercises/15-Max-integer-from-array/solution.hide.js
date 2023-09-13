f54f4559900fa69f6c0aa264723e5d3c828520e0
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 14:58:59 +0200

    Update solution.hide.js

--- exercises/15-Max-integer-from-array/solution.hide.js
+++ exercises/15-Max-integer-from-array/solution.hide.js
@@ -1,14 +1,15 @@
-let myArray = [43, 23, 6, 87, 43, 1, 4, 6, 3, 67, 8, 3445, 3, 7, 5435, 63, 346, 3, 456, 734, 6, 34];
+let myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
 
-function findMax(myArray) {
+function findMax(arr) {
 
-    let maxNum = myArray[0];
+    let maxNum = arr[0];
 
-    for (let i = 1; i < myArray.length; i++) {
-        if (myArray[i] > maxNum)
-            maxNum = myArray[i];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maxNum)
+            maxNum = arr[i];
     }
      
   return maxNum;
 }
+
 console.log(findMax(myArray))
