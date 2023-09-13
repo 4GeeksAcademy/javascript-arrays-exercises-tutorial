f33bbb55610bd5c6c5fec305932b4f9e276aa725
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 18:14:34 +0200

    Update solution.hide.js

--- exercises/20.1-Map-an-array/solution.hide.js
+++ exercises/20.1-Map-an-array/solution.hide.js
@@ -1,8 +1,8 @@
-let arrayOfCelsiusValues = [-2,34,56,-10];
+let arrayOfCelsiusValues = [-2, 34, 56, -10];
 
-let arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value){
+let arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value) {
 	
-	//add your code here and return the new value
+	// Add your code here and return the new value
 	return (value * 9.0/5.0) + 32.0;
 });
 
