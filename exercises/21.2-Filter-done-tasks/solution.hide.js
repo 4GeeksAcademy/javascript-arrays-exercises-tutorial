3cb7b9bdfd78696d3ebb623dfca0273b65303b13
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 21:01:00 +0200

    Create solution.hide.js

---
+++ exercises/21.2-Filter-done-tasks/solution.hide.js
@@ -0,0 +1,18 @@
+let tasks = [
+	{ label: 'Eat my lunch', done: true },
+	{ label: 'Make the bed', done: false },
+	{ label: 'Have some fun', done: false },
+	{ label: 'Finish the replits', done: false },
+	{ label: 'Finish my exercises', done: true },
+	{ label: 'Ask for a raise', done: false },
+	{ label: 'Read a book', done: true },
+	{ label: 'Make a trip', done: false }
+];
+
+// Your code here
+
+let newArray = tasks.filter(function(item) {
+	return item.done === true;
+});
+
+console.log(newArray);
