e27488526cb46dd24b54636210091cc85f2cf13e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 16:20:22 +0200

    Update solution.hide.js

--- exercises/17-The-for-loop/solution.hide.js
+++ exercises/17-The-for-loop/solution.hide.js
@@ -1,9 +1,10 @@
 let myArray = [2323, 4344, 2325, 324413, 21234, 24531, 2123, 42234, 544, 456, 345, 42, 5445, 23, 5656, 423];
 
-let avg = 0;
+// Your code here
+let totalSum = 0;
 for (let i of myArray) {
-	avg += i
+	totalSum += i
 }
-let avgTest = avg / myArray.length
+let avg = totalSum / myArray.length
 
-console.log(avgTest)
+console.log(avg)
