633b48c7a1f4292dd891b49831c37ca390b5e8f5
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 15:02:34 +0200

    Update test.js

--- exercises/15-Max-integer-from-array/test.js
+++ exercises/15-Max-integer-from-array/test.js
@@ -30,7 +30,7 @@ it('You have to use the console.log function', function () {
     const app = require('./app.js');
     expect(console.log.mock.calls.length > 0).toBe(true);
 });
-it('The output in the console should match the one in the instructions!', function () {
+it('The output in the console should match the one in the instructions', function () {
     //You can also compare the entire console buffer (if there have been several console.log calls on the exercise)
     expect(_buffer.includes("5435\n")).toBe(true);
 });
