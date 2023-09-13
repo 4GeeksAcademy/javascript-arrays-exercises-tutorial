044c662a64c2338295603a6d055755cfeb7737e2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-13 20:33:54 +0200

    Create solution.hide.js

---
+++ exercises/21-Filter-an-array/solution.hide.js
@@ -0,0 +1,7 @@
+let allNames = ["Romario", "Boby", "Roosevelt", "Emiliy", "Michael", "Greta", "Patricia", "Danzalee"];
+
+// Your code here
+
+resultingNames = allNames.filter(item=>item[0] === "R")
+
+console.log(resultingNames);
